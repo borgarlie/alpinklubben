5bb819f10073f601cf4cd1b40daa80737009bc8c
Borgar Lie <borgar.lie@gmail.com>
2016-04-22 11:40:46 +0200

    Added setup.py with required files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='alpinklubben',
+    version='',
+    packages=['service', 'entities', 'resources'],
+    url='',
+    license='',
+    author='Borgar',
+    author_email='',
+    description='', requires=['flask', 'flask_appconfig', 'flask_bootstrap', 'flask-login', 'flask-sqlalchemy',
+                              'wtforms']
+)
+
